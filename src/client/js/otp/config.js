8d57a25b1a54fdaf7a48f325de6a48c1b8148d99
p-neves <nevesdiver@gmail.com>
2015-11-25 15:21:18 +0000

    small interface changes

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -111,9 +111,9 @@ otp.config = {
      * Site name / description / branding display options
      */
 
-    siteName            : "My OTP Instance",
+    siteName            : "O meu trip planner",
     siteDescription     : "An OpenTripPlanner deployment.",
-    logoGraphic         : 'images/otp_logo_darkbg_40px.png',
+    logoGraphic         : 'images/mascara.png',
     // bikeshareName    : "",
     //Enable this if you want to show frontend language chooser
     showLanguageChooser : true,
